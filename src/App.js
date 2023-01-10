4dad0a63c653dbcfb18e8fe55b85395294066ea1
SAMY COLLIARD <samy3876@gmail.com>
2023-01-10 10:33:53 +0100

    using css compenant

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes,Route } from 'react-router-dom';
+import Logo from "./logo.svg"
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  background-color: grey;
+`;
+
+const Symbol = styled.img`
+  width: 15vw;
+`;
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+`;
+
+const Img = styled.img`
+  width: 70vw;
+`;
+
+const Liste = styled.li`
+  padding: 10px;
+  justify-content: space-between;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav>
+        <Symbol src={Logo} alt='image of site'></Symbol>
+        <Ul>
+          <Liste>Ceci</Liste>
+          <Liste>Est</Liste>
+          <Liste>Un</Liste>
+          <Liste>Test</Liste>
+        </Ul>
+      </Nav>
+      <h1>Ceci est un Test</h1>
+      <h2>Ceci est toujours un Test</h2>
+      <p>pourquoi? pour tester</p>
+      <Img src={Logo} alt='test'></Img>
     </div>
   );
 }
